be81345d211952894e3966a265b6d6b50fbb038b
Anatoliy A Mukha <bird@dotty.su>
2020-05-17 11:19:13 +0300

    Update example.js

--- example.js
+++ example.js
@@ -1,16 +1,17 @@
 #!/usr/bin/env getDeps
 var d = new Deps();
 
+// Задаем пути для поиска (берет случайнаю библиотеку из найденных)
 d.addSearchPath("/home/android/Qt/5.12.8/Src/qtbase/lib/");
 d.addSearchPath("/usr/lib/gcc/i686-w64-mingw32/");
 d.addSearchPath("/home/android/Qt/5.12.7/Src/gnuwin32/bin/");
 
-d.searchNewLibrary(file);
-
+// запускаем поиск для нужного файла  ($ example.js myapp.exe)
 d.deps(file);
 
-d.print();
+//d.print();
 
+// копируем найденные библиотеки в директорию
 d.copyTo("/tmp/123");
 
 console.log("finish");
